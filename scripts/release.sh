bf590e709d256156ce06bbd480bfa4b7172e8d4f
Johannes Hof <johannes.bortz@gmail.com>
2024-10-16 15:04:14 +0200

    fixed release issues

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,6 +8,14 @@ git restore -s@ -SW  -- packages
 # Build all once to ensure things are nice
 pnpm build
 
+# Update token
+if [[ ! -z ${NPM_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
 # Release packages
 for PKG in packages/* ; do
   pushd $PKG
