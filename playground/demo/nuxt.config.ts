788687ce4b112d7a05ab9c36290ab4fe6be02a55
Johannes Hof <johannes.bortz@gmail.com>
2024-12-03 08:54:22 +0100

    fixed local env vars

--- playground/demo/nuxt.config.ts
+++ playground/demo/nuxt.config.ts
@@ -10,8 +10,8 @@ export default defineNuxtConfig({
     provider: 'medplum',
     config: {
       medplum: {
-        clientId: 'd666f38e-789c-4216-be03-6945490889bf',
-        clientSecret: '467ee05b1d52925997f289d40bb2762f3351337cb325954815fbfc19909c38c6',
+        clientId: process.env.MEDPLUM_CLIENT_ID,
+        clientSecret: process.env.MEDPLUM_CLIENT_SECRET,
         redirectUrl: 'http://localhost:3000/auth/medplum',
         serverUrl: 'http://localhost:8103'
       }
@@ -20,8 +20,8 @@ export default defineNuxtConfig({
 
   fhir: {
     medplum: {
-      clientId: 'd666f38e-789c-4216-be03-6945490889bf',
-      clientSecret: '467ee05b1d52925997f289d40bb2762f3351337cb325954815fbfc19909c38c6',
+      clientId: process.env.MEDPLUM_CLIENT_ID,
+      clientSecret: process.env.MEDPLUM_CLIENT_SECRET,
     }
   },
 
