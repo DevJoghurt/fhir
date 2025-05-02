eec4ab4a025c7cf548d213aeee61f9906bce3e65
Johannes Hof <johannes.bortz@gmail.com>
2025-05-02 12:55:34 +0200

    Form DataTypes: fixed array handling for flat arrays

--- packages/core/src/runtime/app/components/DataType/Form/Helper/Array.vue
+++ packages/core/src/runtime/app/components/DataType/Form/Helper/Array.vue
@@ -3,7 +3,7 @@
 		<div class="flex flex-col space-x-4 space-y-2 pb-2">
 			<template
 				v-for="(val, index) in elementValue"
-				:key="val._id">
+				:key="val?._id || index">
 					<div class="w-full border border-gray-200 rounded-sm p-4">
 						<div class="flex w-full"
 							:class="{
@@ -53,21 +53,30 @@
 	}>()
 	const emit = defineEmits(['update:modelValue', 'change'])
 
-	const elementValue = ref(props.modelValue || [])
-
 	const nestedElements = [] as InternalSchemaElement[]
 	if (props.element.type === 'BackboneElement') {
 		nestedElements.push(...props.element.element || [])
 	}
 
 	// add id to each element in the array
-	if (Array.isArray(elementValue.value)) {
-		elementValue.value.forEach((item, index) => {
-			if (typeof item === 'object' && item !== null) {
-				item._id = Math.random().toString(36).substring(2, 15)
-			}
-		})
-	}
+	const elementValue = computed(() => {
+		if (Array.isArray(props.modelValue)) {
+			return props.modelValue.map((item) => {
+				if (typeof item === 'object' && item !== null) {
+					item._id = Math.random().toString(36).substring(2, 15)
+				}
+				// if it is not an BackboneElement, transform the value to an object
+				if (props.element.type !== 'BackboneElement' && ((typeof item !== 'object') || item === null)) {
+					return {
+						_id: Math.random().toString(36).substring(2, 15),
+						value: item || null
+					}
+				}
+				return item
+			})
+		}
+		return []
+	})
 
 	const removeArrayItem = (index: number) => {
 		// update keys for each element in the array
@@ -87,9 +96,13 @@
 		// TODO: evaluate if this can be a problem in FHIR datatypes
 		const data = JSON.parse(JSON.stringify(elementValue.value))
 		// remove id from each element in the array
-		data.forEach((item) => {
+		data.forEach((item, index) => {
 			if (typeof item === 'object' && item !== null) {
 				delete item._id
+			}
+			// if it is not an BackboneElement, re-transform the object to an flat array
+			if (props.element.type !== 'BackboneElement') {
+				data[index] = item?.value || null
 			}
 		})
 		emit('update:modelValue', data)
