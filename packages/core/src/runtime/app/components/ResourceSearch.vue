008d0d6a6f60c3a6e1e7bed135ae5a85b6392090
Johannes Hof <johannes.bortz@gmail.com>
2025-05-21 14:28:21 +0200

    placeholder resource filter generator

---
+++ packages/core/src/runtime/app/components/ResourceSearch.vue
@@ -0,0 +1,8 @@
+<template>
+
+</template>
+<script lang="ts" setup>
+// Implement the ResourceSearch component: v-model is the search query
+// https://hl7.org/fhir/R4/search.html
+// Respect Modifier of
+</script>
